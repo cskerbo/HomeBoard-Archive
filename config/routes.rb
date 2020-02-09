7a5052d7d5035f7f077be518f58faa8083755b11
cskerbo <chris.kerbo@outlook.com>
2020-02-08 19:26:13 -0800

    Added bootstrap support

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :widgets
   resources :homes
   resources :users
+  root 'static#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
